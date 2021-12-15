f4f972a1dd713eb4d38f77b7bbb91a8009dbf308
Ilolo Izu <iloloizu97@gmail.com>
2021-12-14 19:51:03 -0600

    commit tree 0b410a31950f3276817ab2562012ca419eca4369

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// superbowlWin = (record) => {
+//     const result = record.find( record => record.result === "W" );
+//     return !!result ? result.year : undefined;
+//   }
 
+superbowlWin = (record) => {
+  return (record.find((item) => item.result === 'W')).year
+}
